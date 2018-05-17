2e3ff35038b89b3ea2252341cf72244b1396de6a
Ryan Adelman-Drummond <radelmandrummond@gmail.com>
2018-05-17 08:58:05 -0700

    Set Up
    Made a main and into a java project

---
+++ src/questForFloyda/Main.java
@@ -0,0 +1,16 @@
+/**
+ * 
+ */
+package questForFloyda;
+
+/**
+ * @author Ryan Adelman-Drummond
+ * @version 1.0
+ * @date May 17th, 2018 
+ *
+ */
+public class Main {
+	public static void main(String[] args) {
+	}
+
+}
