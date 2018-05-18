be5a3a2d047190b3e05f590fb661bdde2363ed64
Ryan Adelman-Drummond <radelmandrummond@gmail.com>
2018-05-18 09:17:30 -0700

    Added some frame to the player
    fix #7

---
+++ src/questForFloyda/Player.java
@@ -0,0 +1,94 @@
+package questForFloyda;
+/**
+ * 
+ * @author Ryan Adelman-Drummond
+ * @version 1.0
+ * @date May 18th, 2018
+ *
+ */
+public class Player {
+	/**
+	 * what does every player need to have?
+	 * 	-name
+	 * 	-level
+	 * 	-equipment
+	 * 	-bag
+	 * 	-money
+	 * 	-experience 
+	 * 	-health points
+	 * 	-mana points
+	 */
+	
+	//TODO change all of sting arrays to item arrays
+	//items are objects that need to make
+	private String Name;
+	private int Level;
+	private String[] Bag;
+	private String[] Equipment;
+	private int Money;
+	private int Exp;
+	private int MagicLevel;
+	
+	public Player(String name, int level, String[] bag, String[] equipment, int money, int exp) {
+		Name = name;
+		Level = level;
+		copyArray(Bag, bag);
+		copyArray(Equipment, equipment);
+		Money = money;
+		Exp = exp;
+		MagicLevel = 0;
+	}
+	//copy the array to the other array
+	public void copyArray(String[] CopyTo, String[] CopyThis) {
+		for(int pos = 0; pos < CopyThis.length; pos++) {
+			CopyTo[pos] = CopyThis[pos];
+		}
+	}
+	//Name player stuff
+	public String getPlayerName() {
+		return Name;
+	}
+	
+	//Level player stuff
+	public int getPlayerLevel() {
+		return Level;
+	}
+	public void gainLevel(int gainedLevel) {
+		Level += gainedLevel;
+	}
+	
+	//Magic level player stuff
+	public int getMagicLevel() {
+		return MagicLevel;
+	}
+	public void gainMagicLevel() {
+		MagicLevel++;
+	}
+	
+	//Exp player stuff
+	public int getPlayerExp() {
+		return Exp;
+	}
+	public void gainExp(int gainedExp) {
+		Exp += gainedExp;
+	}
+	
+	//Money player related stuff
+	public int getPlayerMoney() {
+		return Money;
+	}
+	public void addMoney(int plusMoney) {
+		Money += plusMoney;
+	}public void subtractMoeny(int minusMoney) {
+		Money -= minusMoney;
+	}
+	public boolean checkEnough(int cost) {
+		if(Money - cost < 0) {
+		return false;
+		}
+		return true;
+	}
+	//Bag player stuff
+	
+	//Equipment player stuff
+}
