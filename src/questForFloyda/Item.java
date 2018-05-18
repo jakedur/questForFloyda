98ce2a5f8c02bbd4a99f85c6b3a1f85d8b623103
Ryan Adelman-Drummond <radelmandrummond@gmail.com>
2018-05-18 09:20:02 -0700

    added basic item frame
    fix #5

---
+++ src/questForFloyda/Item.java
@@ -0,0 +1,27 @@
+package questForFloyda;
+/**
+ * 
+ * @author Ryan Adelman-Drummond
+ * @version 1.0
+ * @date May 18th, 2018 
+ *
+ */
+public class Item {
+	/**
+	 * what does every item need to have?
+	 * 	-name
+	 * 	-cost
+	 */
+	private String ItemName;
+	private int ItemCost;
+	public Item(String name, int cost) {
+		ItemName = name;
+		ItemCost = cost;
+	}
+	public String getItemName() {
+		return ItemName;
+	}
+	public int getItemCost() {
+		return ItemCost;
+	}
+}
