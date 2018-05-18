3af17252e554bf96037d7ce5d7b9e656ee4c5828
Ryan Adelman-Drummond <radelmandrummond@gmail.com>
2018-05-18 09:29:48 -0700

    add armor frame
    fix #10

---
+++ src/questForFloyda/ArmorItem.java
@@ -0,0 +1,24 @@
+package questForFloyda;
+/**
+ * 
+ * @author Ryan Adelman-Drummond
+ * @version 1.0
+ * @date May 18th, 2018 
+ *
+ */
+public class ArmorItem extends Item{
+	/**
+	 * what does every armor item need?
+	 * 	-name
+	 * 	-cost
+	 * 	-defense
+	 */
+	private int Defense;
+	public ArmorItem(String name, int cost, int defense) {
+		super(name, cost);
+		Defense = defense;
+	}
+	public int getArmorDefense() {
+		return Defense;
+	}
+}
