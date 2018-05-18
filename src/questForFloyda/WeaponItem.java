2da96c785abbb1eaf4b9bf8aabdc6765bcb05e80
Ryan Adelman-Drummond <radelmandrummond@gmail.com>
2018-05-18 09:28:57 -0700

    added weapon frame
    fix #9

---
+++ src/questForFloyda/WeaponItem.java
@@ -0,0 +1,24 @@
+package questForFloyda;
+/**
+ * 
+ * @author Ryan Adelman-Drummond
+ * @version 1.0
+ * @date May 18th, 2018 
+ *
+ */
+public class WeaponItem extends Item{
+	/**
+	 * what does every weapon need?
+	 * 	-name
+	 * 	-cost
+	 * 	-attack
+	 */
+	private int Attack;
+	public WeaponItem(String name, int cost, int attack) {
+		super(name, cost);
+		Attack = attack;
+	}
+	public int getWeaponAttack() {
+		return Attack;
+	}
+}
