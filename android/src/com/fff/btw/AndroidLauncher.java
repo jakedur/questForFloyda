75e9674716f24172a8e72bfd12455b6292a82d58
jakedur <30507841+jakedur@users.noreply.github.com>
2018-05-18 08:25:57 -0700

    I hope this works
    please

---
+++ android/src/com/fff/btw/AndroidLauncher.java
@@ -0,0 +1,16 @@
+package com.fff.btw;
+
+import android.os.Bundle;
+
+import com.badlogic.gdx.backends.android.AndroidApplication;
+import com.badlogic.gdx.backends.android.AndroidApplicationConfiguration;
+import com.fff.btw.questForFloyda;
+
+public class AndroidLauncher extends AndroidApplication {
+	@Override
+	protected void onCreate (Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		AndroidApplicationConfiguration config = new AndroidApplicationConfiguration();
+		initialize(new questForFloyda(), config);
+	}
+}
