6c4579f1dfdc99206b88c53df8e0d4adec3b76ac
Ryan Adelman-Drummond <radelmandrummond@gmail.com>
2018-05-18 14:15:15 -0700

    added some health potion frame
    fix #12

---
+++ src/questForFloyda/HealthPotionItem.java
@@ -0,0 +1,18 @@
+package questForFloyda;
+/**
+ * 
+ * @author Ryan Adelman-Drummond
+ * @version 1.0
+ * @date May 18th, 2018 
+ *
+ */
+public class HealthPotionItem extends Item{
+	private int heal;
+	public HealthPotionItem(String name, int cost, int healthGain) {
+		super(name, cost);
+		heal = healthGain;
+	}
+	public int getHeal() {
+		return heal;
+	}
+}
