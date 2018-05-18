771a0608d914c44c8d90f5d6d06644b1a1845e85
jakedur <30507841+jakedur@users.noreply.github.com>
2018-05-18 15:32:13 -0700

    I hope it works
    dsdf

---
+++ questForFloyda-core/src/com/fff/btw/questForFloyda.java
@@ -0,0 +1,40 @@
+package com.fff.btw;
+
+import com.badlogic.gdx.ApplicationAdapter;
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.graphics.GL20;
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.scenes.scene2d.Stage;
+import com.badlogic.gdx.utils.viewport.ScreenViewport;
+
+public class questForFloyda extends ApplicationAdapter {
+	SpriteBatch batch;
+	Texture img;
+	private Stage stage;
+	
+	@Override
+	public void create () {
+		batch = new SpriteBatch();
+		img = new Texture("twotypes.jpg");
+		stage = new Stage(new ScreenViewport());
+	}
+
+	@Override
+	public void render () {
+		Gdx.gl.glClearColor(1, 0, 0, 1);
+		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
+		
+		batch.begin();
+		batch.draw(img, 0, 0);
+		stage.act(10);
+		stage.clear();
+		batch.end();
+	}
+	
+	@Override
+	public void dispose () {
+		batch.dispose();
+		img.dispose();
+	}
+}
