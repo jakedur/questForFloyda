b247dca171a2aac8d01203da5bd0473f7bbbadf9
Ryan Adelman-Drummond <radelmandrummond@gmail.com>
2018-05-21 17:18:15 -0700

    Added the baisc Bat enemy for the caves
    fix #21

---
+++ core/src/com/mygdx/game/Bat.java
@@ -0,0 +1,63 @@
+package com.mygdx.game;
+
+/**
+ * 
+ * @author Ryan Adelmna-Drummond 
+ * @version 1.0
+ * @date May 21st, 2018
+ * @updated May 21st, 2018
+ */
+public class Bat extends Enemy{
+	/**
+	 * The bat is a low level cave enemy that has low health and medium attack 
+	 */
+	private int MaxHP;
+	private int curHP;
+	private int attack;
+	private int defense;
+	private int expGiven;
+	public Bat(String name) {
+		super(name);
+		MaxHP = 7;
+		curHP = MaxHP;
+		attack = 4;
+		defense = 2;
+		expGiven = 6;
+	}
+	
+	//attack stuff
+	public int getAttack() {
+		return attack;
+	}
+	
+	//defense stuff
+	public int getDefense() {
+		return defense;
+	}
+	
+	//exp stuff
+	public int getExp() {
+		return expGiven;
+	}
+	
+	//health stuff
+	public int getHP() {
+		return curHP;
+	}
+	public void loseHP(int damage) {
+		curHP -= damage;
+	}
+	public void gainHP(int heal) {
+		curHP += heal;
+	}
+	public void FullHP() {
+		curHP = MaxHP;
+	}
+	public boolean death(int damage) {
+		if(curHP - damage < 0) {
+			return true;
+		}
+		return false;
+	}
+
+}
