1c44eb1d650f1b75f1f8d9df79eecd7a773306b4
jakedur <30507841+jakedur@users.noreply.github.com>
2018-05-22 19:20:20 -0700

    fixed crashes and adjusted game map

--- core/src/com/mygdx/game/MyGdxGame.java
+++ core/src/com/mygdx/game/MyGdxGame.java
@@ -48,9 +48,9 @@ public class MyGdxGame extends ApplicationAdapter {
 		
 		if (Gdx.input.justTouched()) {
 			Vector3 pos = cam.unproject(new Vector3(Gdx.input.getX(), Gdx.input.getY(), 0));
-			TileType type = gameMap.getTileTypeByLocation(1, pos.x, pos.y);
+			TileType type = gameMap.getTileTypeByLocation(0, pos.x, pos.y);
 			if (type != null) {
-				//System.out.println("id: " + type.getId() + " name: " + type.getName() + " collidable: " + type.isCollidable() + " damage: " + type.getDamage());
+				System.out.println("id: " + type.getId() + " name: " + type.getName() + " collidable: " + type.isCollidable() + " damage: " + type.getDamage());
 			}
 		}
 		
