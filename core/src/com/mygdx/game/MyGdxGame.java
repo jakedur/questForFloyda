dc50f57aaff0558cdaddb11d5a30dc34e8c1c9e7
jakedur <30507841+jakedur@users.noreply.github.com>
2018-05-24 08:15:49 -0700

    Fixed ids

--- core/src/com/mygdx/game/MyGdxGame.java
+++ core/src/com/mygdx/game/MyGdxGame.java
@@ -46,13 +46,13 @@ public class MyGdxGame extends ApplicationAdapter {
 			//cam.update();
 		//}
 		
-		//if (Gdx.input.justTouched()) {
-			//Vector3 pos = cam.unproject(new Vector3(Gdx.input.getX(), Gdx.input.getY(), 0));
-			//TileType type = gameMap.getTileTypeByLocation(0, pos.x, pos.y);
-			//if (type != null) {
-				//System.out.println("id: " + type.getId() + " name: " + type.getName() + " collidable: " + type.isCollidable() + " damage: " + type.getDamage());
-			//}
-		//}
+		if (Gdx.input.justTouched()) {
+			Vector3 pos = cam.unproject(new Vector3(Gdx.input.getX(), Gdx.input.getY(), 0));
+			TileType type = gameMap.getTileTypeByLocation(0, pos.x, pos.y);
+			if (type != null) {
+				System.out.println("id: " + type.getId() + " name: " + type.getName() + " collidable: " + type.isCollidable() + " damage: " + type.getDamage());
+			}
+		}
 		
 		cam.update();
 		gameMap.update(Gdx.graphics.getDeltaTime());
