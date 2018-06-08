fc5c1638e6975098a4d7c0c4883e985b2b84c0f6
Ryan Adelman-Drummond <radelmandrummond@gmail.com>
2018-06-07 22:50:30 -0700

    added a empty item manegment screen needs lot of work

---
+++ core/src/screens/ItemManegmentScreen.java
@@ -0,0 +1,54 @@
+package screens;
+
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.graphics.GL20;
+import com.badlogic.gdx.scenes.scene2d.Stage;
+import com.badlogic.gdx.scenes.scene2d.ui.Table;
+import com.mygdx.game.Quest;
+
+public class ItemManegmentScreen extends AbstractScreen{
+	private Quest apps;
+	private Stage uiStage;
+	private Table root;
+	public ItemManegmentScreen(Quest app) {
+		super(app);
+		apps = app;
+	}
+
+	@Override
+	public void show() {
+		
+	}
+
+	@Override
+	public void render(float delta) {
+		Gdx.gl.glClearColor(0f, 0f, 252f, 1f);
+		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
+	}
+
+	@Override
+	public void resize(int width, int height) {
+		
+	}
+
+	@Override
+	public void pause() {
+		
+	}
+
+	@Override
+	public void resume() {
+		
+	}
+
+	@Override
+	public void hide() {
+		
+	}
+
+	@Override
+	public void dispose() {
+		
+	}
+
+}
